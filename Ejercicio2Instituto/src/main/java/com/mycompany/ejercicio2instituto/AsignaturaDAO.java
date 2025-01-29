bd06f49181ecac504b3475ece93d861ef9b33ada
Claudia <claudissperez99@gmail.com>
2025-01-29 14:43:49 +0100

    haciendo

--- Ejercicio2Instituto/src/main/java/com/mycompany/ejercicio2instituto/AsignaturaDAO.java
+++ Ejercicio2Instituto/src/main/java/com/mycompany/ejercicio2instituto/AsignaturaDAO.java
@@ -4,26 +4,87 @@
  */
 package com.mycompany.ejercicio2instituto;
 
+import java.util.ArrayList;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
 /**
  *
  * @author Claudia
  */
 public class AsignaturaDAO {
-    private static void addAsignatura(){
+    private static void addAsignatura(Asignatura asignatura){
+        Session session = Conexion.getSession();
         
+        Transaction transaction = null;
+        
+        try{
+            //Iniciar transaccion
+            transaction = session.beginTransaction();
+            session.persist(asignatura);
+            transaction.commit();
+            System.out.println("Asignatura guardada con exito");
+            
+        }catch(Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }finally{
+            Conexion.close();
+        }
     }
     
-    private static void updateAsignatura(){
+    private static void updateAsignatura(Asignatura asignatura){
+        Session session = Conexion.getSession();
+        Transaction transaction = null;
         
+        try{
+            transaction = session.beginTransaction();
+            session.merge(asignatura);
+            transaction.commit();
+            System.out.println("Asignatura actualizada con éxito");
+            
+        }catch(Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }finally{
+            Conexion.close();
+        }
     }
     
-    private static void deleteAsignatura(){
+    private static void deleteAsignatura(int id){
+        Session session = Conexion.getSession();
+        Transaction transaction = null;
         
+        try{
+            transaction = session.beginTransaction();
+            Asignatura asignatura = searchAsignaturaByID(id);
+            session.remove(asignatura);
+            transaction.commit();
+            System.out.println("Asignatura borrada correctamente");
+        }catch(Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }finally{
+            Conexion.close();
+            
+        }
     }
-    private static void ListarAsignaturas(){
-        
+    private static ArrayList<Asignatura> ListarAsignaturas(){
+        Session session = Conexion.getSession();
+        return (ArrayList<Asignatura>) session.createQuery("FROM Asignatura", Asignatura.class).getResultList();
     }
-    private static void searchAsignaturaByID(){
-        
+    private static Asignatura searchAsignaturaByID(int id){
+        Session session = Conexion.getSession();
+        Query query = session.createQuery("FROM Asignatura WHERE id= :id", Asignatura.class);
+        query.setParameter("id", id);
+        Asignatura asinatura = (Asignatura) query.uniqueResult();
+        return asinatura;
     }
 }
--- Ejercicio2Instituto/src/main/java/com/mycompany/ejercicio2instituto/ProfesorDAO.java
+++ Ejercicio2Instituto/src/main/java/com/mycompany/ejercicio2instituto/ProfesorDAO.java
@@ -7,6 +7,7 @@ package com.mycompany.ejercicio2instituto;
 import java.util.ArrayList;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 /**
  *
@@ -33,19 +34,70 @@ public class ProfesorDAO {
         }
     }
     
-    private static void updateProfesor(){
+    private static void updateProfesor(Profesor profesor){
+        Session session = Conexion.getSession();
+        Transaction transaction = null;
         
+        try{
+            //Iniciar transaccion 
+            transaction= session.beginTransaction();
+            session.merge(profesor);
+            transaction.commit();
+            System.out.println("Profesor actualizado con exito");
+            
+        }catch(Exception e){
+            if (transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }finally{
+            Conexion.close();
+        }
     }
     
-    private static void deleteProfesor(){
+    private static void deleteProfesor(int idProfesor){
+        //Primero hay que borrar todas las asignaturas cuyo profesor sea el que queremos eliminar
+        Session session = Conexion.getSession();
         
+        Transaction transaction = null;
+        
+        try{
+            //Iniciar transaccion 
+            transaction = session.beginTransaction();
+            Profesor profesor = searchProfesorByID(idProfesor);
+            //Buscamos las asignaturas que da ese profe
+            Query query = session.createQuery("FROM Asignatura WHERE profesor= :profesor ", Asignatura.class);
+            query.setParameter("profesor", profesor);
+            ArrayList<Asignatura> asignaturas = (ArrayList<Asignatura>) query.getResultList();
+            
+            for (Asignatura a: asignaturas){
+                //Borramos las asignturas que imparte el profe
+                session.remove(a);
+            }
+            
+            //Borramos profe
+            session.remove(profesor);
+            transaction.commit();
+            System.out.println("Profe borrado");
+        }catch(Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }finally{
+            Conexion.close();
+        }
     }
     private static ArrayList<Profesor> ListarProfesores(){
         Session session = Conexion.getSession();
         return (ArrayList<Profesor>) session.createQuery("FROM profesor", Profesor.class);
         
     }
-    private static void searchProfesorByID(){
-        
+    private static Profesor searchProfesorByID(int id){
+        Session session = Conexion.getSession();
+        Query query = session.createQuery("FROM Profesor WHERE id= :id", Profesor.class);
+        query.setParameter("id", id);
+        Profesor profesor = (Profesor) query.uniqueResult();
+        return profesor;
     }
 }
