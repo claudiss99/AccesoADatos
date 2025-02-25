0c7e3093f51acb3e7016affad611a079a29dfb61
Claudia <claudissperez99@gmail.com>
2025-02-25 08:25:56 +0100

    subiendo

--- Mongoose/libreria.js
+++ Mongoose/libreria.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const app = express();
+
 const mongoose = require('mongoose');
 
 // Conectar a MongoDB
@@ -8,39 +7,15 @@ mongoose.connect('mongodb+srv://claudiss99:claudiss648912204@insti.aldna.mongodb
   .catch((err) => console.log('Error de conexión:', err));
 
 
-//Modificar un libro
-app.post('/libros/', (req, res) => {
-    const titulo = req.body.titulo;
-    const autor = req.body.Autor;
-    const year = req.body.year;
-    const editorial =req.body.editorial;
-    res.send("Modificado libro con éxito")
-  });
-
 // Definir un esquema
 const productoSchema = new mongoose.Schema({
   titulo: { type: String, required: true },
-  Autor: { type: String, required: true },
-  year: { type: int, default: 0 },
+  autor: { type: String, required: true },
+  year: { type: Number, default: 0 },
   editorial: {type:String, required:true}
 });
 
 // Crear un modelo a partir del esquema
-const Producto = mongoose.model('Libreria', productoSchema);
+const Libro = mongoose.model('Libro', productoSchema);
 
-// Crear un nuevo producto
-const nuevoProducto = new Producto({
-  nombre: 'Camiseta',
-  precio: 20,
-  stock: 50
-});
-
-// Guardar el producto en la base de datos
-nuevoProducto.save()
-  .then(() => console.log('Producto guardado en la base de datos'))
-  .catch((err) => console.log('Error al guardar el producto:', err));
-
-// Consultar todos los productos
-Producto.find()
-  .then((productos) => console.log(productos))
-  .catch((err) => console.log('Error al consultar productos:', err));
+module.exports = {Libro};
--- Mongoose/libros.js
+++ Mongoose/libros.js
@@ -1,37 +1,68 @@
 const express = require('express');
 const app = express();
 
+const {Libro} = require('./libreria.js');
+const PORT = process.env.PORT||4000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log('Servidor corriendo en el puerto ${PORT}');
+});
+
+
+
 //Crear un libro
-app.post('/libros/', (req, res) => {
+app.post('/libros', (req, res) => {
+    const libro = req.body;
+    const nuevoLibro = new Libro(libro);
+    nuevoLibro.save();
     res.send('libro creado con exito');
   });
 
-//Modificar libro
-app.post('/libros/:id', (req, res) => {
-    const libroId = req.params.id;
-    res.send(`Modificación del libro con ID: ${libroId}`);
-  });
 
+//Modificar un libro
+app.put('/libros/:id', (req, res) => {
+  const body = req.body;
+  Libro.findByIdAndUpdate(req.params.id, {
+    titulo: body.titulo,
+    autor: body.Autor,
+    year: body.year,
+    editorial: body.editorial
+  })
+  .then((libro) => res.send('Libro actualizado correctamente'))
+  .catch((err) => res.send('Error al actualizar el libro '+err))
+});
 
 //Eliminar un libro
 app.delete('/libros/:id', (req, res) => {
-    res.send('Libro borrado con éxito');
+  Libro.findByIdAndDelete(req.params.id)
+  .then((libro) => res.send(libro ? 'Libro borrado correctamente': 'Libro no encontrado'))
+  .catch((err) => res.send('Error al actualizar el libro '+err))
+
 });
 
 //Consultar todos los libros
-app.get('/libros/', (req, res) => {
-  res.send(`Consultando todos los libros`);
+app.get('/libros', (req, res) => {
+  Libro.find()
+  .then((libros) => res.send(libros))
+  .catch((err) => res.send('Error al consultar: ', err))
 });
 
 //Consultar libro por id
 app.get('/libros/:id', (req, res) => {
-  // Acceder a la variable de la ruta
-  const libroId = req.params.id;
-  res.send(`Información del libro con ID: ${libroId}`);
+  Libro.findById(req.params.id)
+  .then((libros) => res.send(libros))
+  .catch((err) => res.send('Error al consultar por id: ', err))
 });
 
+//Consultar libro por fragmento
+app.get('/libros/fragmento', (req, res) => {
+  const fragmento = req.query.fragmento;
+  const regex = new RegExp(fragmento, 'i'):
+  Libro.find({titulo: {$regex:regex}})
+  .then((libros) => res.send(libros))
+  .catch((err) => res.send('Error consultar por fragmento: ',err))
+})
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+
